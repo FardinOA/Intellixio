73f4a2ee5ffbd6c0353ce542cbab254df5ab66c6
Fardin Omor Afnan <66800888+FardinOA@users.noreply.github.com>
2024-10-28 06:39:22 +0600

    Revert "Fix: Implemented challenges"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
 import { UserAgentProvider } from "../components/providers/userAgentProvider";
 import "./globals.css";
 import { Layout } from "@/components/layout";
-import { headers } from "next/headers";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const userAgent = headers().get("user-agent") || "Unknown User Agent";
-
-    return (
-        <html lang="en">
-            <body>
-                <UserAgentProvider userAgent={userAgent}>
-                    <Layout>{children}</Layout>
-                </UserAgentProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <UserAgentProvider>
+          <Layout>{children}</Layout>
+        </UserAgentProvider>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
